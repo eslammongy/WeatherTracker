87478a21f599853378e343943b97cd331fb2fe1d
Eslam Mongy <47952686+eslammongy@users.noreply.github.com>
2024-10-22 14:31:09 +0300

    updating api constant

--- lib/core/constants/api_constant.dart
+++ lib/core/constants/api_constant.dart
@@ -1,4 +1,4 @@
 const String baseUrl = "https://api.weatherbit.io/v2.0/";
-const String appKey = "a85e7129c4c44ff99e90adfc65394748";
+const String appKey = "put your api key here";
 const String forecastEndPoint = "forecast/daily";
 const String currentEndPoint = "forecast/";
